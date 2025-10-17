7983ca47dc86d77cafce9cd0cf03181e56900cdc
Autumnmaple1 <hongyiming000003@163.com>
2025-10-17 12:48:53 +0800

    fix

--- message_deal.py
+++ message_deal.py
@@ -28,9 +28,9 @@ def parse_xml(xml_content, compstr, type):
     try:
         root = ET.fromstring(xml_content)
         # 提取消息内容
-        if type == "1":
+        if type == 1:
             reply_content = root.findtext(compstr, default="").strip()
-        if type == "2":
+        if type == 2:
             reply_content = root.get(compstr)
         return reply_content
     except ET.ParseError as e:
@@ -92,6 +92,11 @@ def message_process(message_content, local_type, wxid):
         message_content = "[卡片分享] "+content
         return message_content
     
+    if local_type == 17179869233:
+        content = parse_xml(message_content,".//title",1)
+        message_content = "[卡片分享] " + content
+        return message_content
+    
     if local_type == 8589934592049:
         message_content = "[微信转账]"
         return message_content
@@ -101,7 +106,7 @@ def message_process(message_content, local_type, wxid):
         return message_content
     
     if local_type == 42:
-        message_content = "[名片]"
+        message_content = "[名片] " + parse_xml(message_content,"nickname",2)
         return message_content
     if local_type == 10000:
         return "[系统消息]"
